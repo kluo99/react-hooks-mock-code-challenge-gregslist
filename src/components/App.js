638a540656d9b77ee7edfc0bc3944ceafd3c9282
Kenneth <luo.kenneth1999@gmail.com>
2023-11-28 14:42:26 -0500

    finished challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [dataList, setDataList] = useState([])
+  const [search, setSearch] = useState('')
+
+  console.log(search)
+  console.log("bye")
+  console.log(dataList.description)
+  console.log("hello")
+
+  // dataList.forEach(item => {
+  //   console.log(dataList);
+  //   console.log('hello')
+  // });
+
+
+  
+
+  // console.log(sortedLocation);
+ 
+  useEffect(() => {
+    fetch ('http://localhost:6001/listings')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setDataList(data)
+    })
+  }, [])
+
+  // useEffect(() => {
+  //   filterDataList();
+  // }, [itemDelete])
+
+  // function passListingToApp(listing) {
+  //   setItemDelete(listing)
+  // }
+
+  // function filterDataList() {
+  //   const newArray = dataList.filter((item) => item.includes(itemDelete))
+  // }
+
+
+  function setData(newArray) {
+    setDataList(newArray)
+  }
+
+  function changeSearch(textInput) {
+    console.log(textInput)
+    setSearch(textInput)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header changeSearch={changeSearch} />
+      
+      <ListingsContainer dataList={dataList} setData={setData} search={search}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,25 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ dataList, setData, passListingToApp, search }) {
+  console.log(dataList)
+
+  // function deleteListings(listingId) {
+  //   setDataList(dataList.filter((item) => listingId !== item.id))
+  // }
+
+  const searchedText = dataList.filter((item) => item.description.includes(search))
+
+  //sorting alphabetically
+  const sortedLocation = [...searchedText].sort((a,b) => {
+    return a.location.localeCompare(b.location);
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {sortedLocation.map((listing) => <ListingCard dataList={dataList} setData={setData} listing={listing} key={listing.description} passArrayToApp={passListingToApp} />)
+      }
       </ul>
     </main>
   );
